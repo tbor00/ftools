2c19e9f5ba778ab8f5171fb4def5fecb782189dc
tbor00 <btimoteo@outlook.com>
2022-09-18 18:39:49 -0300

    add an optional options

--- packages/network/src/axios.js
+++ packages/network/src/axios.js
@@ -1,23 +1,23 @@
 import axios from 'axios'
 import { methods, request } from './base'
 
-const get = async (url, options) => {
+const get = async (url, options = {}) => {
     return request(methods.GET, url, undefined, options, axios)
 }
 
-const post = async (url, body, options) => {
+const post = async (url, body, options = {}) => {
     return request(methods.POST, url, body, options, axios)
 }
 
-const put = async (url, body, options) => {
+const put = async (url, body, options = {}) => {
     return request(methods.PUT, url, body, options, axios)
 }
 
-const patch = async (url, body, options) => {
+const patch = async (url, body, options = {}) => {
     return request(methods.PATCH, url, body, options, axios)
 }
 
-const deleteReq = async (url, options) => {
+const deleteReq = async (url, options = {}) => {
     return request(methods.DELETE, url, undefined, options, axios)
 }
 
