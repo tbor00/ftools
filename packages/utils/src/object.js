0306ef28880bb5d6a7380a84e52bd50f46cc51c2
tbor00 <btimoteo@outlook.com>
2022-10-05 02:22:13 -0300

    feat(utils): isObject function was added

--- packages/utils/src/object.js
+++ packages/utils/src/object.js
@@ -1,4 +1,11 @@
 /**
+ * If the value is not null and the type of the value is an object, then return true, otherwise return
+ * false.
+ * @param {unknown} value - The value to check.
+ */
+export const isObject = (value) => value !== null && typeof value === 'object'
+
+/**
  * It returns true if the object is empty, false if it's not
  * @param {unknown} obj - The object to check if it's empty.
  * @returns {boolean} A boolean value.
