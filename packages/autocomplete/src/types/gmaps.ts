4fa9fc5b1ef9061ebbfe02f4afbb73a638d9a400
tbor00 <tborgob@gmail.com>
2022-10-13 21:38:32 -0300

    feat: add more types of gmaps places

--- packages/autocomplete/src/types/gmaps.ts
+++ packages/autocomplete/src/types/gmaps.ts
@@ -6,8 +6,17 @@ export type GeoCoderResult = google.maps.GeocoderResult
 export type AutocompletePredictions = google.maps.places.AutocompletePrediction
 export type PlacesServiceStatus = `${google.maps.places.PlacesServiceStatus | ''}`
 export type AutocompleteService = google.maps.places.AutocompleteService
+export type PlacesService = google.maps.places.PlacesService
+export type PlaceDetailsReq = google.maps.places.PlaceDetailsRequest
+export type PlaceResult = google.maps.places.PlaceResult
 
 export interface Gmaps {
     GeoCoderReques: GeoCoderRequest
     GeoCoderResult: GeoCoderResult
+    AutocompletePredictions: AutocompletePredictions
+    PlacesServiceStatus: PlacesServiceStatus
+    AutocompleteService: AutocompleteService
+    PlacesService: PlacesService
+    PlaceDetailsReq: PlaceDetailsReq
+    PlaceResult: PlaceResult
 }
