811cc0fc67f26e5d16c45fe252ed8e32160f103f
tbor00 <tborgob@gmail.com>
2022-11-05 03:44:59 -0300

    feat: add comments to explain memoized method

--- packages/utils/src/cache.js
+++ packages/utils/src/cache.js
@@ -1,3 +1,9 @@
+/**
+ * It takes a function as an argument and returns a function that caches the result of the original
+ * function
+ * @param fnCallback - The function that we want to memoize.
+ * @returns A function that takes in a number and returns the fibonacci number at that index.
+ */
 export function memoized(fnCallback) {
     let cache = {}
     return (args) => {
