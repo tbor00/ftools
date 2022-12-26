fe58449167b25c48ef74476b06cdfb344e117e89
tbor00 <tborgob@gmail.com>
2022-12-26 12:42:40 -0300

    tests(utils): add new use cases to tests

--- packages/utils/tests/object.test.js
+++ packages/utils/tests/object.test.js
@@ -1,11 +1,36 @@
-import { objectIsEmpty } from '../src/object'
+import { objectIsEmpty, isObject, objectContains } from '../src/object'
 
-describe('objectIsEmpty helper', () => {
-    it('if we pass an empty object it should return true', () => {
-        expect(objectIsEmpty({})).toBe(true)
+describe('object module', () => {
+    describe('isObject', () => {
+        it('when the parameter is object, should return true', () => {
+            expect(isObject({})).toBeTruthy()
+        })
+        it('when the parameter is not object, should return false', () => {
+            expect(isObject('')).toBeFalsy()
+        })
     })
 
-    it('if we pass an object with properties it should return false', () => {
-        expect(objectIsEmpty({ propertyOne: 'Property' })).toBe(false)
+    describe('objectIsEmpty', () => {
+        it('if we pass an empty object it should return true', () => {
+            expect(objectIsEmpty({})).toBe(true)
+        })
+
+        it('if we pass an object with properties it should return false', () => {
+            expect(objectIsEmpty({ propertyOne: 'Property' })).toBe(false)
+        })
+    })
+
+    describe('objectContains', () => {
+        const objMock = {
+            username: 'userNameExample'
+        }
+
+        it('when the key exist on object, should return true', () => {
+            expect(objectContains(objMock, 'username')).toBeTruthy()
+        })
+
+        it('when the key not exist on object, should return false', () => {
+            expect(objectContains(objMock, 'name')).toBeFalsy()
+        })
     })
 })
--- packages/utils/tests/processing.test.js
+++ packages/utils/tests/processing.test.js
@@ -1,39 +1,87 @@
-import { removeAccents, bytesConvert, fileSizeByteToMb, getQueryParamsWithProxy } from '../src/processing'
+import { removeAccents, bytesConvert, fileSizeByteToMb, getQueryParamsWithProxy, isUndef, isFunction, isBoolean, isString } from '../src/processing'
 
-describe('processing files work correctly', () => {
-    test('bytesConver is defined', () => {
-        expect(bytesConvert).toBeDefined()
+describe('processing module', () => {
+    describe('processing files work correctly', () => {
+        test('bytesConver is defined', () => {
+            expect(bytesConvert).toBeDefined()
+        })
+
+        test('bytesConver work as it should', () => {
+            expect(bytesConvert(500)).toBe('500 Bytes')
+            expect(bytesConvert(80000)).toBe('78.1 KB')
+            expect(bytesConvert(100000000)).toBe('95.4 MB')
+        })
+
+        test('fileSizeByteToMb is defined', () => {
+            expect(fileSizeByteToMb).toBeDefined()
+            expect(fileSizeByteToMb(150000000)).toBe(143)
+        })
+
+        test('fileSizeByteToMb work as it should', () => {
+            expect(fileSizeByteToMb(150000000)).toBe(143)
+        })
     })
 
-    test('bytesConver work as it should', () => {
-        expect(bytesConvert(500)).toBe('500 Bytes')
-        expect(bytesConvert(80000)).toBe('78.1 KB')
-        expect(bytesConvert(100000000)).toBe('95.4 MB')
+    test('removeAccents method work as it should', () => {
+        expect(removeAccents('Ananá')).toBe('Anana')
     })
 
-    test('fileSizeByteToMb is defined', () => {
-        expect(fileSizeByteToMb).toBeDefined()
-        expect(fileSizeByteToMb(150000000)).toBe(143)
-    })
-
-    test('fileSizeByteToMb work as it should', () => {
-        expect(fileSizeByteToMb(150000000)).toBe(143)
-    })
-})
-
-test('removeAccents method work as it should', () => {
-    expect(removeAccents('Ananá')).toBe('Anana')
-})
-
-test('getQueryParams work correctly', () => {
-    global.window = {
-        location: {
-            search: {
-                id: '2000'
+    test('getQueryParams work correctly', () => {
+        global.window = {
+            location: {
+                search: {
+                    id: '2000'
+                }
             }
         }
-    }
-    const { id, isReal } = getQueryParamsWithProxy()
-    expect(id).toBe('2000')
-    expect(isReal).toBeNull()
+        const { id, isReal } = getQueryParamsWithProxy()
+        expect(id).toBe('2000')
+        expect(isReal).toBeNull()
+    })
+
+    describe('isUndef', () => {
+        test('when the parameter is undefined, should return true', () => {
+            expect(isUndef(undefined)).toBeTruthy()
+        })
+
+        test('when the parameter is not undefined, should return false', () => {
+            expect(isUndef({})).toBeFalsy()
+        })
+    })
+
+    describe('isString', () => {
+        test('when the parameter is string, should return true', () => {
+            expect(isString('Im a string')).toBeTruthy()
+        })
+
+        test('when the parameter is not string, should return false', () => {
+            expect(isString({})).toBeFalsy()
+        })
+    })
+
+    describe('isBoolean', () => {
+        test('when the parameter is boolean, should return true', () => {
+            expect(isBoolean(true)).toBeTruthy()
+            expect(isBoolean(false)).toBeTruthy()
+        })
+
+        test('when the parameter is not boolean, should return false', () => {
+            expect(isBoolean({})).toBeFalsy()
+            expect(isBoolean('')).toBeFalsy()
+            expect(isBoolean(undefined)).toBeFalsy()
+        })
+    })
+
+    describe('isFunction', () => {
+        test('when the parameter is a function, should return true', () => {
+            const fn = () => console.warn('This is a mock function')
+            expect(isFunction(fn)).toBeTruthy()
+        })
+
+        test('when the parameter is not a function, should return false', () => {
+            expect(isFunction({})).toBeFalsy()
+            expect(isFunction('')).toBeFalsy()
+            expect(isFunction(true)).toBeFalsy()
+        })
+    })
 })
