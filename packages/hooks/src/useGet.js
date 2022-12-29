20e859f14f2bcb81c9c9aa85402f718a83a96620
LouieMartin <louie.decierdo@gmail.com>
2022-12-30 00:34:37 +0800

    Added loading to the `useGet` hook

--- packages/hooks/src/useGet.js
+++ packages/hooks/src/useGet.js
@@ -4,16 +4,25 @@ import { useEffect, useState } from 'react'
  *  This hook returns the json response of a request
  * @param {RequestInfo | URL} url - The url to fetch the json response.
  * @param {RequestInit} options - The options given to the fetch.
+ * @returns An array with three elements. First element is the response element
+ * it returns null if either loading or error, Second element is the loading element
+ * returns true if the fetch promise has not returned anything yet, Third element
+ * is the error element it doesn't return null if the request returned an error
  */
-export default function (url, options) {
+export default function useGet(url, options) {
     const [response, setResponse] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch(url, options)
-            .then(res => setResponse(res))
-            .catch(err => setError(err))
+            .then(res => res.json())
+            .then(data => {
+                setLoading(false)
+                setResponse(data)
+            })
+            .catch(err => setError(err));
     }, [])
 
-    return [response, error];
+    return [response, loading, error];
 }
